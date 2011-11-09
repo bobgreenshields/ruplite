596c2dcac2ef718ce0637ac5e4e628c1d3afd97a
Robert Greenshields <bob@thegreenshields.com>
2011-11-09 22:39:58 +0000

    initial object def

---
+++ lib/ruplite.rb
@@ -0,0 +1,21 @@
+class NullLogger
+	def initialize
+		@absorb = [:info, :debug, :warn, :fatal]
+	end
+
+	def method_missing(meth, *args, &blk)
+		if @absorb.include? meth
+			return nil
+		else
+			super
+		end
+	end
+end
+
+class Ruplite
+	def initialize (config, logger = nil)
+		@logger = logger || NullLogger.new
+		@config = config
+	end
+
+end
