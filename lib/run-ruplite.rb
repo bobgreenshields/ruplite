d9c507ee43f5606e05ef935afb97bb7f8a27716a
Robert Greenshields <bob@thegreenshields.com>
2011-11-18 22:37:23 +0000

    reorganise run script

--- lib/run-ruplite.rb
+++ lib/run-ruplite.rb
@@ -1,16 +1,20 @@
 require './ruplite'
 require 'logger'
 
-src = '/home/bobg/icon'
-tgt = 'file:///home/bobg/backups/test'
+config = {}
+
+config[:source] = '/home/bobg/icon'
+config[:target] = 'file:///home/bobg/backups/test'
+config[:passphrase] = 'xxx'
+config[:options] = ['--encrypt-key 87909562', '--sign-key 31894F89']
+
 log = Logger.new(STDOUT)
 
-opts = []
-opts << '--encrypt-key 87909562'
-opts << '--sign-key 31894F89'
-
-config = {:source => src, :target => tgt, :options => opts}
-config[:passphrase] = 'xxx'
+#opts = []
+#opts << '--encrypt-key 87909562'
+#opts << '--sign-key 31894F89'
+#
+#config = {:source => src, :target => tgt, :options => opts}
 
 #test = Ruplite.new('Test', config)
 test = Ruplite.new('Test', config, log)
